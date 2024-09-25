2fa10c0a6586c0a27b8c8b85ffb41ccc30f56852
ali-consciousroad <ali_dindar@live.be>
2024-09-25 14:35:37 +0200

    Separation of concern: separate the view and logic for the Fruits component - right version

--- src/Fruits.js
+++ src/Fruits.js
@@ -1,6 +1,7 @@
 function Fruits(props) {
     // Map over all the fruits of the fruits array and return the fruitName value for each fruit. 
     const fruitNameItem = props.myFruits.map(f => <p key={f.id}>{f.fruitName}</p>)
+    // Test
     console.log(fruitNameItem); 
     return (
         <div>
